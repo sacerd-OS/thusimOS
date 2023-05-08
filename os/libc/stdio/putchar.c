7d75bb31b72bb23f5e49afcd8ead5825d5607fb0
citrus <augustalexander27@gmail.com>
2023-05-08 18:34:25 -0400

    atoi

--- os/libc/stdio/putchar.c
+++ os/libc/stdio/putchar.c
@@ -2,8 +2,16 @@
 #include "../etc/terminal.h"
 
 int putchar(const char c) {
+	// for scrolling if necessary
 	size_t line = 0;
+	
+	// we dont want to print \0
 	if(c == '\0') return -1;
+	
+	// \n case:
+	// move to next row
+	// if next row is off the screen, scroll
+	// and delete the last line to make room
     if(c == '\n'){
 	  column = 0;
 	  row++;
@@ -16,7 +24,17 @@ int putchar(const char c) {
 	  }
 	  return 1;
     }
+	
+	// this line is generally the only one that'll run
+	// it baically just prints a char(c) in a color (terminal_color)
+	// at a coordinate (column, row)
 	vga_putentryat(c, terminal_color, column, row);
+	
+	// case: character hits the end of the screen
+	// move to next row
+	// if next row is off the screen, scroll
+	// and delete the last row to make room
+	// note: VERY similar to \n case
 	if (++column == VGA_WIDTH) {
 		column = 0;
 		if (++row == VGA_HEIGHT){
